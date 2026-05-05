40b08d8fcf5489f78b6ebca497e9843e066fb28d
pcouy <contact@pierre-couy.dev>
2026-05-05 11:20:40 +0200

    [rmkit] More kobo building related stuff
    - Add kobo/ output dir to .gitignore
    - Fix `make remux_docker` and other similar targets for kobo => Build
      is a lot faster than rebuilding everything everytime

--- scripts/build/docker_build.sh
+++ scripts/build/docker_build.sh
@@ -2,10 +2,11 @@ OUTDIR=artifacts
 PACKAGE="${1}"
 TARGET=${TARGET:-rm}
 FBINK=${FBINK}
+CROSS_TC=${CROSS_TC:-arm-linux-gnueabihf}
 mkdir ${OUTDIR}
 docker run -i --rm -v "${PWD}/${OUTDIR}:/mnt/artifacts" rmkit:${TARGET} /bin/bash << COMMANDS
 mkdir -p src/build
-FBINK=${FBINK} TARGET=${TARGET} make ${PACKAGE}
+CROSS_TC=${CROSS_TC} FBINK=${FBINK} TARGET=${TARGET} make ${PACKAGE}
 mkdir -p /mnt/artifacts/${TARGET}/
 cp -r src/build/* /mnt/artifacts/${TARGET}/
 chown -R $(id -u):$(id -u) /mnt/artifacts
